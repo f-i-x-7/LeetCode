eb9cdc23949379ebe79fbc41c55492237a56bd54
Artem Eliseev <64615981+f-i-x-7@users.noreply.github.com>
2022-04-04 17:27:54 +0300

    Swapping nodes in a linked list https://leetcode.com/problems/swapping-nodes-in-a-linked-list/

---
+++ Medium/SwappingNodesInALinkedList_001721/SwappingNodesInALinkedList_001721/CircularBuffer.cs
@@ -0,0 +1,342 @@
+﻿using System.Collections;
+
+namespace SwappingNodesInALinkedList_001721
+{
+    /// <summary>
+    /// Circular buffer.
+    /// </summary>
+    /// <remarks>
+    /// This class is not thread-safe.<br/>
+    /// When writing to a full buffer:<br/>
+    /// <see cref="PushBack(T)"/> -> removes this[0] / <see cref="PeekFront()"/>;<br/>
+    /// <see cref="PushFront(T)"/> -> removes this[<see cref="Size"/>-1] / <see cref="PeekBack()"/>().
+    /// </remarks>
+    public sealed class CircularBuffer<T> : IEnumerable<T>
+    {
+        private readonly T[] buffer;
+
+        /// <summary>
+        /// The _start. Index of the first element in buffer.
+        /// </summary>
+        private int start;
+
+        /// <summary>
+        /// The _end. Index after the last element in the buffer.
+        /// </summary>
+        private int end;
+
+        /// <summary>
+        /// The _size. Buffer size.
+        /// </summary>
+        private int size;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CircularBuffer{T}"/> class.
+        /// </summary>
+        /// <param name='capacity'>
+        /// Buffer capacity. Must be positive.
+        /// </param>
+        public CircularBuffer(int capacity)
+            : this(capacity, Array.Empty<T>())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CircularBuffer{T}"/> class.
+        /// </summary>
+        /// <param name='capacity'>
+        /// Buffer capacity. Must be positive.
+        /// </param>
+        /// <param name='items'>
+        /// Items to fill buffer with. Items length must be less than or equal to capacity.
+        /// </param>
+        public CircularBuffer(int capacity, T[] items)
+        {
+            if (capacity < 1)
+            {
+                throw new ArgumentException(
+                    "Circular buffer cannot have negative or zero capacity.", nameof(capacity));
+            }
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+            if (items.Length > capacity)
+            {
+                throw new ArgumentException(
+                    "Too many items to fit circular buffer", nameof(items));
+            }
+
+            buffer = new T[capacity];
+
+            Array.Copy(items, buffer, items.Length);
+            size = items.Length;
+
+            start = 0;
+            end = size == capacity ? 0 : size;
+        }
+
+        /// <summary>
+        /// Maximum capacity of the buffer. Elements pushed into the buffer after
+        /// maximum capacity is reached (<see cref="IsFull"/> = <see langword="true"/>), will remove an element.
+        /// </summary>
+        public int Capacity => buffer.Length;
+
+        /// <summary>
+        /// Boolean indicating if Circular is at full capacity.
+        /// Adding more elements when the buffer is full will
+        /// cause elements to be removed from the other end
+        /// of the buffer.
+        /// </summary>
+        public bool IsFull => Size == Capacity;
+
+        /// <summary>
+        /// <see langword="true"/> if has no elements; otherwise, <see langword="false"/>.
+        /// </summary>
+        public bool IsEmpty => Size == 0;
+
+        /// <summary>
+        /// Current buffer size (the number of elements that the buffer has).
+        /// </summary>
+        public int Size => size;
+
+        /// <summary>
+        /// Element at the front of the buffer - this[0].
+        /// </summary>
+        /// <returns>The element at the front of the buffer.</returns>
+        public T PeekFront()
+        {
+            ThrowIfEmpty();
+            return buffer[start];
+        }
+
+        /// <summary>
+        /// Element at the back of the buffer - this[<see cref="Size"/> - 1].
+        /// </summary>
+        /// <returns>The element at the back of the buffer.</returns>
+        public T PeekBack()
+        {
+            ThrowIfEmpty();
+            return buffer[(end != 0 ? end : Capacity) - 1];
+        }
+
+        /// <summary>
+        /// Index access to elements in buffer.
+        /// Index does not loop around like when adding elements,
+        /// valid interval is [0; Size).
+        /// </summary>
+        /// <param name="index">Index of element to access.</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown when index is outside of [0; Size) interval.</exception>
+        public T this[int index]
+        {
+            get
+            {
+                if (IsEmpty)
+                {
+                    ThrowIndexOutOfRangeException($"Cannot access index {index}. Buffer is empty.");
+                }
+                if (index >= size)
+                {
+                    ThrowIndexOutOfRangeException($"Cannot access index {index}. Buffer size is {size}.");
+                }
+                var actualIndex = InternalIndex(index);
+                return buffer[actualIndex];
+            }
+            set
+            {
+                if (IsEmpty)
+                {
+                    ThrowIndexOutOfRangeException($"Cannot access index {index}. Buffer is empty.");
+                }
+                if (index >= size)
+                {
+                    ThrowIndexOutOfRangeException($"Cannot access index {index}. Buffer size is {size}.");
+                }
+                var actualIndex = InternalIndex(index);
+                buffer[actualIndex] = value;
+            }
+        }
+
+        /// <summary>
+        /// Pushes a new element to the back of the buffer. <see cref="PeekBack()"/>/this[<see cref="Size"/>-1]
+        /// will now return this element.
+        /// 
+        /// When the buffer is full, the element at <see cref="PeekFront()"/>/this[0] will be 
+        /// popped to allow for this new element to fit.
+        /// </summary>
+        /// <param name="item">Item to push to the back of the buffer.</param>
+        public void PushBack(T item)
+        {
+            if (IsFull)
+            {
+                buffer[end] = item;
+                Increment(ref end);
+                start = end;
+            }
+            else
+            {
+                buffer[end] = item;
+                Increment(ref end);
+                ++size;
+            }
+        }
+
+        /// <summary>
+        /// Pushes a new element to the front of the buffer. <see cref="PeekFront()"/>/this[0]
+        /// will now return this element.
+        /// 
+        /// When the buffer is full, the element at <see cref="PeekBack()"/>/this[<see cref="Size"/>-1] will be 
+        /// popped to allow for this new element to fit.
+        /// </summary>
+        /// <param name="item">Item to push to the front of the buffer</param>
+        public void PushFront(T item)
+        {
+            if (IsFull)
+            {
+                Decrement(ref start);
+                end = start;
+                buffer[start] = item;
+            }
+            else
+            {
+                Decrement(ref start);
+                buffer[start] = item;
+                ++size;
+            }
+        }
+
+        /// <summary>
+        /// Removes the element at the back of the buffer. Decreasing the 
+        /// Buffer size by 1.
+        /// </summary>
+        /// <returns>Removed element at the back of the buffer.</returns>
+        public T PopBack()
+        {
+            ThrowIfEmpty();
+            Decrement(ref end);
+            var result = buffer[end];
+            buffer[end] = default(T);
+            --size;
+            return result;
+        }
+
+        /// <summary>
+        /// Removes the element at the front of the buffer. Decreasing the 
+        /// Buffer size by 1.
+        /// </summary>
+        /// <returns>Removed element at the front of the buffer.</returns>
+        public T PopFront()
+        {
+            ThrowIfEmpty();
+            var result = buffer[start];
+            buffer[start] = default(T);
+            Increment(ref start);
+            --size;
+            return result;
+        }
+
+        #region IEnumerable<T> implementation
+
+        /// <summary>
+        /// Returns an enumerator that iterates through this buffer.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate this collection.</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            var firstSegment = ArraySegmentOne();
+            var secondSegment = ArraySegmentTwo();
+            for (var i = 0; i < firstSegment.Count; i++)
+            {
+                yield return firstSegment.Array[firstSegment.Offset + i];
+            }
+            for (var i = 0; i < secondSegment.Count; i++)
+            {
+                yield return secondSegment.Array[secondSegment.Offset + i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        #endregion
+
+        private static void ThrowIndexOutOfRangeException(string message) => throw new IndexOutOfRangeException(message);
+
+        private void ThrowIfEmpty()
+        {
+            if (IsEmpty)
+            {
+                Throw();
+            }
+
+
+            static void Throw() => throw new InvalidOperationException("Cannot take element from empty buffer.");
+        }
+
+        /// <summary>
+        /// Increments the provided index variable by one, wrapping
+        /// around if necessary.
+        /// </summary>
+        /// <param name="index"></param>
+        private void Increment(ref int index)
+        {
+            if (++index == Capacity)
+            {
+                index = 0;
+            }
+        }
+
+        /// <summary>
+        /// Decrements the provided index variable by one, wrapping
+        /// around if necessary.
+        /// </summary>
+        /// <param name="index"></param>
+        private void Decrement(ref int index)
+        {
+            if (index == 0)
+            {
+                index = Capacity;
+            }
+            index--;
+        }
+
+        /// <summary>
+        /// Converts the index in the argument to an index in <code>_buffer</code>
+        /// </summary>
+        /// <returns>
+        /// The transformed index.
+        /// </returns>
+        /// <param name='index'>
+        /// External index.
+        /// </param>
+        private int InternalIndex(int index)
+        {
+            return start + (index < (Capacity - start) ? index : index - Capacity);
+        }
+
+        private ArraySegment<T> ArraySegmentOne()
+        {
+            if (IsEmpty)
+            {
+                return new ArraySegment<T>(Array.Empty<T>());
+            }
+            if (start < end)
+            {
+                return new ArraySegment<T>(buffer, offset: start, count: end - start);
+            }
+            return new ArraySegment<T>(buffer, offset: start, count: buffer.Length - start);
+        }
+
+        private ArraySegment<T> ArraySegmentTwo()
+        {
+            if (IsEmpty)
+            {
+                return new ArraySegment<T>(Array.Empty<T>());
+            }
+            if (start < end)
+            {
+                return new ArraySegment<T>(buffer, offset: end, count: 0);
+            }
+            return new ArraySegment<T>(buffer, offset: 0, count: end);
+        }
+    }
+}
---
+++ Medium/SwappingNodesInALinkedList_001721/SwappingNodesInALinkedList_001721/Program.cs
@@ -0,0 +1,288 @@
+﻿using SwappingNodesInALinkedList_001721;
+
+https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+
+// You are given the head of a linked list, and an integer k.
+// Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end
+// (the list is 1-indexed).
+
+// Example 1:
+// Input: head = [1, 2, 3, 4, 5], k = 2
+// Output:[1,4,3,2,5]
+
+// Example 2:
+// Input: head = [7, 9, 6, 6, 7, 8, 3, 0, 9, 5], k = 5
+// Output:[7,9,6,6,8,7,3,0,9,5]
+
+
+// Constraints:
+// The number of nodes in the list is n.
+// 1 <= k <= n <= 10^5
+// 0 <= Node.val <= 100
+
+
+
+// Samples from task definition
+Test(source: new[] { 1, 2, 3, 4, 5 }, k: 2, expectedResult: new[] { 1, 4, 3, 2, 5 }); // swap non-adjacent non-head & non-tail nodes
+Test(source: new[] { 7, 9, 6, 6, 7, 8, 3, 0, 9, 5 }, k: 5, expectedResult: new[] { 7, 9, 6, 6, 8, 7, 3, 0, 9, 5 }); // swap adjacent non-head & non-tail nodes
+
+// Own test cases
+Test(source: new[] { 1 }, k: 1, expectedResult: new[] { 1 }); // no swapping
+Test(source: new[] { 1, 2 }, k: 1, expectedResult: new[] { 2, 1 }); // swap adjacent head and tail
+Test(source: new[] { 1, 2 }, k: 2, expectedResult: new[] { 2, 1 }); // swap adjacent head and tail, mirrored 'k'
+Test(source: new[] { 1, 2, 3 }, k: 1, expectedResult: new[] { 3, 2, 1 }); // swap non-adjacent head and tail
+Test(source: new[] { 1, 2, 3 }, k: 3, expectedResult: new[] { 3, 2, 1 }); // swap non-adjacent head and tail, mirrored 'k'
+
+// Same as from task definition but k is mirrored
+Test(source: new[] { 1, 2, 3, 4, 5 }, k: 4, expectedResult: new[] { 1, 4, 3, 2, 5 });  // swap non-adjacent non-head & non-tail nodes, mirrored 'k'
+Test(source: new[] { 7, 9, 6, 6, 7, 8, 3, 0, 9, 5 }, k: 6, expectedResult: new[] { 7, 9, 6, 6, 8, 7, 3, 0, 9, 5 }); // swap adjacent non-head & non-tail nodes, mirrored 'k'
+
+static void Test(int[] source, int k, int[] expectedResult)
+{
+    var result = Swap(source, k);
+    var equal = Enumerable.SequenceEqual(expectedResult, result);
+
+    if (!equal)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"FAILED: k = {k}, Source = {ToString(source)}, Expected result = {ToString(expectedResult)}, Actual result = {ToString(result)}");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.WriteLine($"SUCCESS: k = {k}, Source = {ToString(source)}, Result = {ToString(result)}");
+    }
+
+
+    static string ToString(int[] arr) => $"[{string.Join(",", arr)}]";
+}
+
+static int[] Swap(int[] items, int k)
+{
+    var resultAsLinkedList = Solution.SwapNodes(ConvertToLinkedList(items), k);
+
+    var result = new int[items.Length];
+    var i = 0;
+    var node = resultAsLinkedList;
+    while (node != null)
+    {
+        result[i] = node.val;
+        node = node.next;
+        i++;
+    }
+
+    return result;
+
+
+    static ListNode ConvertToLinkedList(int[] items)
+    {
+        var node = new ListNode(items[0]);
+        var head = node;
+        var i = 1;
+
+        while (i < items.Length)
+        {
+            node.next = new ListNode(items[i]);
+            node = node.next;
+            i++;
+        }
+
+        return head;
+    }
+}
+
+
+
+
+
+
+// Definition for singly - linked list.
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public static class Solution
+{
+    public static ListNode SwapNodes(ListNode head, int k)
+    {
+        if (k <= 0)
+            throw new ArgumentOutOfRangeException(nameof(k));
+        if (head is null)
+            throw new ArgumentException(nameof(head));
+
+        // O(N) time complexity, O(k) additional adjacent space required (because of 'k+1' size of circular buffer), seemed most optimal here.
+        // Other non-optimal strategies that came into mind:
+        // 1) Materialize linked list into List<T> (requires O(N) additional adjacent space), then easily find k-th element from behind. Still O(N) time complexity.
+        // 2) Convert singly linked list into double linked list, determine its count, then to define k-th item from tail either iterate from tail or from head (depends on k/N value).
+        // O(N) additional space, time complexity up to O(N + N/2)
+        var (predecessorOfToSwapFromStart, predecessorOfToSwapFromEnd, tail) = IterateLinkedList(head, k);
+
+        var toSwapFromStart = predecessorOfToSwapFromStart is null ? head : predecessorOfToSwapFromStart.next;
+        var toSwapFromEnd = predecessorOfToSwapFromEnd is null ? head : predecessorOfToSwapFromEnd.next;
+        
+        var result = Swap(head: head,
+            tail: tail,
+            node1: toSwapFromStart,
+            node2: toSwapFromEnd,
+            nodeBeforeNode1: predecessorOfToSwapFromStart,
+            nodeBeforeNode2: predecessorOfToSwapFromEnd);
+
+        return result;
+
+
+
+        static (ListNode predecessorOfToSwapFromStart, ListNode predecessorOfToSwapFromEnd, ListNode tail) IterateLinkedList(ListNode head, int k)
+        {
+            ListNode current = head;
+            int i = 1;
+            ListNode previous = null;
+            ListNode predecessorOfToSwapFromStart = null;
+            ListNode predecessorOfToSwapFromEnd = null;
+
+            // We can use circular buffer of capacity 'k' and push every node during iteration into its back.
+            // Then, in the end, front of buffer will contain k-th item from tail of linked list.
+            // But as our linked list is singly, then we need also predecessor of k-th item from tail.
+            // So use 'k+1' capacity, and do not forget that 'k+1' can be greater than N.
+            CircularBuffer<ListNode> lastKPlusOneNodes = new(k + 1);
+
+            while (current != null)
+            {
+                if (i == k)
+                {
+                    predecessorOfToSwapFromStart = previous;
+                }
+
+                lastKPlusOneNodes.PushBack(current);
+
+                previous = current;
+                current = current.next;
+                i++;
+            }
+
+            System.Diagnostics.Debug.Assert(lastKPlusOneNodes.Size >= 1);
+            if (lastKPlusOneNodes.Size == k + 1)
+            {
+                predecessorOfToSwapFromEnd = lastKPlusOneNodes.PeekFront();
+            }
+            else
+            {
+                // It is found out that k == N
+                System.Diagnostics.Debug.Assert(lastKPlusOneNodes.Size == k);
+                predecessorOfToSwapFromEnd = null;
+            }
+
+            return (predecessorOfToSwapFromStart: predecessorOfToSwapFromStart, predecessorOfToSwapFromEnd: predecessorOfToSwapFromEnd, tail: lastKPlusOneNodes.PeekBack());
+        }
+
+        // Return result is head of new list
+        static ListNode Swap(ListNode head, ListNode tail, ListNode node1, ListNode node2, ListNode nodeBeforeNode1, ListNode nodeBeforeNode2)
+        {
+            System.Diagnostics.Debug.Assert(nodeBeforeNode1 == null || nodeBeforeNode1.next == node1);
+            System.Diagnostics.Debug.Assert(nodeBeforeNode2 == null || nodeBeforeNode2.next == node2);
+
+            if (node1 == node2)
+                return head; // No swapping
+
+            var nodeAfterNode1BeforeSwap = node1.next;
+            var nodeAfterNode2BeforeSwap = node2.next;
+
+            if (node1 == head)
+            {
+                System.Diagnostics.Debug.Assert(nodeBeforeNode1 == null, "because node1 is head");
+
+                if (node2 == tail)
+                {
+                    System.Diagnostics.Debug.Assert(nodeAfterNode2BeforeSwap == null, "because node2 is tail");
+                    System.Diagnostics.Debug.Assert(nodeBeforeNode2 != null, "because node2 is tail and node1 != node2");
+
+                    // Swapping head & tail
+                    if (head.next == tail)
+                    {
+                        // Adjacent head & tail
+                        head.next = null;
+                        tail.next = head;
+                    }
+                    else
+                    {
+                        // Non-adjacent head & tail
+                        node1.next = null;
+                        node2.next = nodeAfterNode1BeforeSwap; // TODO: fails here if head & tail are adjacent (cyclic reference)
+                        nodeBeforeNode2.next = node1;
+                    }
+                    return tail;
+                }
+
+                System.Diagnostics.Debug.Fail("In this task it is not possible to swap head/tail with non-head/non-tail");
+            }
+
+            if (node2 == head)
+            {
+                System.Diagnostics.Debug.Assert(nodeBeforeNode2 == null, "because node2 is head");
+
+                if (node1 == tail)
+                {
+                    System.Diagnostics.Debug.Assert(nodeAfterNode1BeforeSwap == null, "because node1 is tail");
+                    System.Diagnostics.Debug.Assert(nodeBeforeNode1 != null, "because node1 is tail and node1 != node2");
+
+                    // Swapping head & tail
+                    if (head.next == tail)
+                    {
+                        // Adjacent head & tail
+                        head.next = null;
+                        tail.next = head;
+                    }
+                    else
+                    {
+                        node2.next = null;
+                        node1.next = nodeAfterNode2BeforeSwap; // TODO: fails here if head & tail are adjacent (cyclic reference)
+                        nodeBeforeNode1.next = node2;
+                    }
+                    return tail;
+                }
+
+                System.Diagnostics.Debug.Fail("In this task it is not possible to swap head/tail with non-head/non-tail");
+            }
+
+            // Swapping nodes are not head and/or tail. Head of linked list is not modified
+            if (nodeBeforeNode2 == node1)
+            {
+                // Swapping nodes are adjacent: ... -> node1 -> node2 -> ...
+                // Should be:                   ... -> node2 -> node1 -> ...
+                nodeBeforeNode1.next = node2;
+                node2.next = node1;
+                node1.next = nodeAfterNode2BeforeSwap;
+
+                return head;
+            }
+
+            if (nodeBeforeNode1 == node2)
+            {
+                // Swapping nodes are adjacent: ... -> node2 -> node1 -> ...
+                // Should be:                   ... -> node1 -> node2 -> ...
+                nodeBeforeNode2.next = node1;
+                node1.next = node2;
+                node2.next = nodeAfterNode1BeforeSwap;
+
+                return head;
+            }
+
+            // Swapping nodes are adjacent: ... -> node1 -> ... -. node2 -> ...
+            // Should be:                   ... -> node2 -> ... -> node1 -> ...
+            // Or vice versa (same code will work)
+            nodeBeforeNode1.next = node2;
+            node2.next = nodeAfterNode1BeforeSwap; //will be cyclic reference when node1.next == node2!
+
+            nodeBeforeNode2.next = node1;
+            node1.next = nodeAfterNode2BeforeSwap;
+
+            return head;
+        }
+    }
+}
