336cba99cc35decde23761c5e1576f8c36e83bf0
Artem Eliseev <64615981+f-i-x-7@users.noreply.github.com>
2022-04-18 22:05:10 +0300

    Increasing Order Search Tree - another approaches https://leetcode.com/problems/increasing-order-search-tree/

--- Easy/IncreasingOrderSearchTree_000897/IncreasingOrderSearchTree_000897/Program.cs
+++ Easy/IncreasingOrderSearchTree_000897/IncreasingOrderSearchTree_000897/Program.cs
@@ -15,7 +15,13 @@
 //The number of nodes in the given tree will be in the range [1, 100].
 //0 <= Node.val <= 1000
 
-Console.WriteLine("Hello, World!");
+
+
+var root = new TreeNode(5, left: new TreeNode(1), right: new TreeNode(7));
+//var root = new TreeNode(5, left: new TreeNode(1));
+var result = new Solution().IncreasingBST(root);
+
+Console.WriteLine("FINISHED");
 
 
 public class TreeNode
@@ -35,9 +41,14 @@ public class Solution
 {
     public TreeNode IncreasingBST(TreeNode root)
     {
+        return IncreasingBST_RecursiveWithInPlaceProcessing_2(root);
+    }
+
+    private TreeNode IncreasingBST_RecursiveWithPostProcessing(TreeNode root)
+    {
         // Do depth-first search and place all nodes into queue, then process nodes extracted from queue
         // Time: O(N) - need to process all nodes
-        // Space: O(N) - all nodes are placed into queue, and algo call stack will require from O(H) to O(N) space
+        // Space: O(N) - all nodes are placed into queue, and also call stack requires O(H) space
 
         var orderedNodes = new Queue<TreeNode>();
         Process(root, orderedNodes);
@@ -58,7 +69,7 @@ public class Solution
         current.left = null;
         current.right = null;
         return newRoot;
-        
+
 
         static void Process(TreeNode node, Queue<TreeNode> orderedNodes)
         {
@@ -68,6 +79,82 @@ public class Solution
             Process(node.left, orderedNodes);
             orderedNodes.Enqueue(node);
             Process(node.right, orderedNodes);
+        }
+    }
+
+    private TreeNode IncreasingBST_RecursiveWithInPlaceProcessing(TreeNode root)
+    {
+        // Peeked at Leetcode solution.
+        // Traverse tree with depth-first search in-order, and append each visiting node into 'linked list'
+        // Time: O(N) - need to process all nodes
+        // Space: O(H) - for call stack
+        var fake = new TreeNode(int.MinValue); // node holding min possible value, it would be the 1st node in transformed tree
+        var current = fake;
+        Process(root, ref current);
+        return fake.right; // skip fake node
+
+
+        // 'current' is last node in transformed tree (aka last node in result 'linked list')
+        static void Process(TreeNode node, ref TreeNode current)
+        {
+            if (node == null)
+                return;
+
+            Process(node.left, ref current);
+
+            // Left child was already visited, it is safe to break link.
+            // And it is required to clear left link in visiting node, this automatically clears left link for last node in result 'linked list'
+            // (e.g. imagine BST tree with root 5 and left node 1; if 'current.left = null' was written than link 5 -> 1 would be preserved.
+            // And with this line, we always have 'current.left == null' equal to true:
+            // 1) fake root (initially 'current' points to it) has null left node
+            // 2) all real nodes appending to 'linked list' have cleared left child before append
+            node.left = null;
+
+            current.right = node; // append node being visited to 'linked list'
+            current = node;
+            Process(node.right, ref current);
+        }
+    }
+
+    private TreeNode IncreasingBST_RecursiveWithInPlaceProcessing_2(TreeNode root)
+    {
+        // Inspired by previous solution, but no fake node introduced.
+        TreeNode newRoot = null;
+        TreeNode current = null;
+        Process(root, ref current, ref newRoot);
+        System.Diagnostics.Debug.Assert(newRoot != null);
+        return newRoot;
+
+
+        // 'current' is last node in transformed tree (aka last node in result 'linked list')
+        static void Process(TreeNode node, ref TreeNode current, ref TreeNode newRoot)
+        {
+            if (node == null)
+                return;
+
+            Process(node.left, ref current, ref newRoot);
+
+            if (newRoot == null)
+            {
+                // We are visiting the smallest value in BST
+                System.Diagnostics.Debug.Assert(current == null);
+                newRoot = node;
+                current = node;
+            }
+            else
+            {
+                // Left child was already visited, it is safe to break link.
+                // And it is required to clear left link in visiting node, this automatically clears left link for last node in result 'linked list'
+                // (e.g. imagine BST tree with root 5 and left node 1; if 'current.left = null' was written than link 5 -> 1 would be preserved.
+                // And with this line, we always have 'current.left == null' equal to true:
+                // 1) node holding the smallest value in BST (initially 'current' points to it) has null left node
+                // 2) all real nodes appending to 'linked list' have cleared left child before append
+                node.left = null;
+
+                current.right = node; // append node being visited to 'linked list'
+                current = node;
+            }
+            Process(node.right, ref current, ref newRoot);
         }
     }
 }
