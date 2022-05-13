0d41271f1d3db588ce61824f3904c4d7ebea4cfb
Artem Eliseev <64615981+f-i-x-7@users.noreply.github.com>
2022-05-14 00:10:41 +0300

    Populating Next Right Pointers in Each Node II https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/

---
+++ Medium/PopulatingNextRightPointersInEachNode2_000117/PopulatingNextRightPointersInEachNode2_000117/Program.cs
@@ -0,0 +1,105 @@
+﻿// https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+
+//Given a binary tree
+
+//struct Node
+//{
+//    int val;
+//    Node* left;
+//    Node* right;
+//    Node* next;
+//}
+//Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+//Initially, all next pointers are set to NULL.
+
+
+//Example 1:
+//Input: root = [1, 2, 3, 4, 5, null, 7]
+//Output: [1,#,2,3,#,4,5,7,#]
+//Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+
+//Example 2:
+//Input: root = []
+//Output: []
+
+//Constraints:
+//The number of nodes in the tree is in the range [0, 6000].
+//-100 <= Node.val <= 100
+
+
+
+//Follow - up:
+
+//You may only use constant extra space.
+//The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+// Definition for a Node.
+public class Node
+{
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node()
+    {
+    }
+
+    public Node(int _val)
+    {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next)
+    {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+
+public class Solution
+{
+    public Node Connect(Node root)
+    {
+        if (root != null)
+        {
+            // Breadth-first traversal.
+            // Time: O(N) - need to visit all nodes at each level
+            // Space: O(N) (O(N) to store nodes, O(H) for call stack)
+            Traverse(new[] { root });
+        }
+        return root;
+    }
+
+    private void Traverse(IList<Node> nodesAtPreviousLevel)
+    {
+        var nodesAtCurrentLevel = new List<Node>();
+
+        foreach (var node in nodesAtPreviousLevel)
+        {
+            if (node.left != null)
+                nodesAtCurrentLevel.Add(node.left);
+            if (node.right != null)
+                nodesAtCurrentLevel.Add(node.right);
+        }
+
+        if (nodesAtCurrentLevel.Count == 0)
+            return;
+
+        for (var i = 0; i < nodesAtCurrentLevel.Count - 1; i++)
+        {
+            nodesAtCurrentLevel[i].next = nodesAtCurrentLevel[i + 1];
+        }
+
+        Traverse(nodesAtCurrentLevel);
+    }
+}
