ac2db5a4bdc4b8865b83169504fc44efca84b991
Artem Eliseev <64615981+f-i-x-7@users.noreply.github.com>
2022-05-21 13:05:15 +0300

    Unique Paths II - another approach https://leetcode.com/problems/unique-paths-ii/

--- Medium/UniquePaths2_000063/UniquePaths2_000063/Program.cs
+++ Medium/UniquePaths2_000063/UniquePaths2_000063/Program.cs
@@ -38,6 +38,11 @@ public class Solution
 
     public int UniquePathsWithObstacles(int[][] obstacleGrid)
     {
+        return UniquePathsWithObstacles_DFS(obstacleGrid);
+    }
+
+    private int UniquePathsWithObstacles_DFS(int[][] obstacleGrid)
+    {
         if (obstacleGrid == null || obstacleGrid.Length == 0 || obstacleGrid[0] is null || obstacleGrid[0].Length == 0)
             return 0;
 
@@ -83,5 +88,57 @@ public class Solution
 
             return numPaths;
         }
+    }
+
+    private int UniquePathsWithObstacles_DP(int[][] obstacleGrid)
+    {
+        if (obstacleGrid == null || obstacleGrid.Length == 0 || obstacleGrid[0] is null || obstacleGrid[0].Length == 0)
+            return 0;
+
+        var m = obstacleGrid.Length;
+        var n = obstacleGrid[0].Length;
+
+        if (obstacleGrid[0][0] != SPACE || obstacleGrid[m - 1][n - 1] != SPACE)
+            return 0;
+
+        // DP approach, taken from solution section from Leetcode.
+        // Time: O(m*n) - each cell will be visited only once
+        // Space: O(m*n) - can be reduced to O(1) if current grid can be modified.
+
+        var dp = new int[m][]; // contains number of ways to arrive from starting cell to (i,j) cell
+        for (var i = 0; i < m; i++)
+        {
+            dp[i] = new int[n];
+        }
+
+        // Iterate over first row and first column, fill dp grid.
+        // This is because we can arrive into cells of first row only if robot always moves left (for first column - always moves down).
+        for (var j = 0; j < n; j++)
+        {
+            if (obstacleGrid[0][j] == SPACE)
+                dp[0][j] = 1;
+            else
+                break;
+        }
+
+        for (var i = 0; i < m; i++)
+        {
+            if (obstacleGrid[i][0] == SPACE)
+                dp[i][0] = 1;
+            else
+                break;
+        }
+
+        // Then iterate over non-filled cells by rows (e.g. starting from (1,1) cell), fill dp grid as sum of dp cells from above and left.
+        for (var i = 1; i < m; i++)
+        {
+            for (var j = 1; j < n; j++)
+            {
+                if (obstacleGrid[i][j] == SPACE)
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 }
